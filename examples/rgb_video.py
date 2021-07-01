82cbaeef48fc935bb8f60d01a14fc226ee678613
alex-luxonis <alex@luxonis.com>
2021-07-01 04:06:22 +0300

    Debug camera latency and lost frames. Average host FPS over a sliding window
    Example output:
    Host FPS:  0.00. Camera seqnum: 1, latency:  44.548 ms. Lost 1 frames
    Host FPS:  6.57. Camera seqnum: 5, latency:  63.243 ms. Lost 3 frames
    Host FPS: 11.95. Camera seqnum: 6, latency:  44.968 ms
    Host FPS: 15.14. Camera seqnum: 7, latency:  42.404 ms
    Host FPS: 17.13. Camera seqnum: 8, latency:  44.300 ms
    Host FPS: 18.83. Camera seqnum: 9, latency:  43.017 ms
    Host FPS: 20.08. Camera seqnum: 10, latency:  42.903 ms
    Host FPS: 21.07. Camera seqnum: 11, latency:  42.894 ms
    Host FPS: 21.87. Camera seqnum: 12, latency:  43.031 ms
    Host FPS: 22.56. Camera seqnum: 13, latency:  42.814 ms
    Host FPS: 23.13. Camera seqnum: 14, latency:  42.862 ms
    Host FPS: 31.91. Camera seqnum: 15, latency:  42.724 ms
    Host FPS: 30.11. Camera seqnum: 16, latency:  43.202 ms
    Host FPS: 29.91. Camera seqnum: 17, latency:  42.926 ms
    Host FPS: 30.09. Camera seqnum: 18, latency:  42.788 ms
    Host FPS: 29.91. Camera seqnum: 19, latency:  43.436 ms
    Host FPS: 29.96. Camera seqnum: 20, latency:  42.862 ms

--- examples/rgb_video.py
+++ examples/rgb_video.py
@@ -2,6 +2,8 @@
 
 import cv2
 import depthai as dai
+import time
+import collections
 
 # Create pipeline
 pipeline = dai.Pipeline()
@@ -17,19 +19,54 @@ camRgb.setBoardSocket(dai.CameraBoardSocket.RGB)
 camRgb.setResolution(dai.ColorCameraProperties.SensorResolution.THE_1080_P)
 camRgb.setVideoSize(1920, 1080)
 
-xoutVideo.input.setBlocking(False)
-xoutVideo.input.setQueueSize(1)
+# Disabling this would lead to latency build up with USB2
+if 1:
+    xoutVideo.input.setBlocking(False)
+    xoutVideo.input.setQueueSize(1)
 
 # Linking
 camRgb.video.link(xoutVideo.input)
+
+
+# Calculates FPS over a moving window, configurable
+class FPS:
+    def __init__(self, window_size=10):
+        self.dq = collections.deque(maxlen=window_size)
+        self.fps = 0
+
+    def add(self, timestamp=None):
+        if timestamp == None: timestamp = time.monotonic()
+        count = len(self.dq)
+        if count > 0: self.fps = count / (timestamp - self.dq[0])
+        self.dq.append(timestamp)
+
+    def get(self):
+        return self.fps
+
 
 # Connect to device and start pipeline
 with dai.Device(pipeline) as device:
 
     video = device.getOutputQueue(name="video", maxSize=1, blocking=False)
 
+    seq_prev = -1
+    fps = FPS()
     while True:
+        # For testing, read packets with a delay
+        if 0: time.sleep(0.5)
+
         videoIn = video.get()
+
+        fps.add()
+        seqnum = videoIn.getSequenceNum()
+        tstamp = videoIn.getTimestamp().total_seconds()
+        latency = (time.monotonic() - tstamp) * 1000
+        seq_diff = seqnum - seq_prev
+        seq_prev = seqnum
+        print(f"Host FPS: {fps.get():5.2f}. ", end="")
+        print(f"Camera seqnum: {seqnum}, latency: {latency:7.3f} ms", end="")
+        if seq_diff != 1: print(f". Lost {seq_diff - 1} frames", end="")
+        print()
 
         # Get BGR frame from NV12 encoded video frame to show with opencv
         # Visualizing the frame on slower hosts might have overhead
