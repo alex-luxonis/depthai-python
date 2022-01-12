4c6705380ce3ff2008094a3f87284baf8861a594
alex-luxonis <60824841+alex-luxonis@users.noreply.github.com>
2022-01-12 08:15:02 +0200

    rgb_encoding.py: print performance data
    Run also with `DEPTHAI_LEVEL=info` to get CPU load

--- examples/VideoEncoder/rgb_encoding.py
+++ examples/VideoEncoder/rgb_encoding.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 
 import depthai as dai
+import time
 
 # Create pipeline
 pipeline = dai.Pipeline()
@@ -21,12 +22,31 @@ videoEnc.setDefaultProfilePreset(30, dai.VideoEncoderProperties.Profile.H265_MAI
 camRgb.video.link(videoEnc.input)
 videoEnc.bitstream.link(xout.input)
 
+# Calculates FPS over a moving window, configurable
+class FPS:
+    def __init__(self, window_size=30):
+        import collections
+        self.dq = collections.deque(maxlen=window_size)
+        self.fps = 0
+
+    def add(self, timestamp=None):
+        if timestamp == None: timestamp = time.monotonic()
+        count = len(self.dq)
+        if count > 0: self.fps = count / (timestamp - self.dq[0])
+        self.dq.append(timestamp)
+
+    def get(self):
+        return self.fps
+
 # Connect to device and start pipeline
 with dai.Device(pipeline) as device:
 
     # Output queue will be used to get the encoded data from the output defined above
     q = device.getOutputQueue(name="h265", maxSize=30, blocking=True)
 
+    seq_prev = -1
+    fps = FPS()
+    
     # The .h265 file is a raw stream file (not playable yet)
     with open('video.h265', 'wb') as videoFile:
         print("Press Ctrl+C to stop encoding...")
@@ -34,6 +54,18 @@ with dai.Device(pipeline) as device:
             while True:
                 h265Packet = q.get()  # Blocking call, will wait until a new data has arrived
                 h265Packet.getData().tofile(videoFile)  # Appends the packet data to the opened file
+                
+                fps.add()
+                seqnum = h265Packet.getSequenceNum()
+                tstamp = h265Packet.getTimestamp().total_seconds()
+                latency = (time.monotonic() - tstamp) * 1000
+                seq_diff = seqnum - seq_prev
+                seq_prev = seqnum
+                print(f"Host FPS: {fps.get():5.2f}. ", end="")
+                print(f"Camera seqnum: {seqnum}, latency: {latency:7.3f} ms", end="")
+                if seq_diff != 1: print(f". Lost {seq_diff - 1} frames", end="")
+                print()
+                
         except KeyboardInterrupt:
             # Keyboard interrupt (Ctrl + C) detected
             pass
