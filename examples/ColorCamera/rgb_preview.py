f62c293abbf43588c01bf6a92f1db0445bfc3093
alex-luxonis <60824841+alex-luxonis@users.noreply.github.com>
2022-03-05 21:30:20 +0200

    FPS helper, using host or device timestamps

--- examples/ColorCamera/rgb_preview.py
+++ examples/ColorCamera/rgb_preview.py
@@ -20,6 +20,36 @@ camRgb.setColorOrder(dai.ColorCameraProperties.ColorOrder.RGB)
 # Linking
 camRgb.preview.link(xoutRgb.input)
 
+# updateInterval: in seconds
+# name: if provided, will be verbose, printing FPS when it updates
+class FPS:
+    def __init__(self, updateInterval=1, name=None):
+        self.fps = 0
+        self.count = 0
+        self.timePrev = None
+        self.interval = updateInterval
+        self.name = name
+
+    # timestamp: optional to not use current time (but e.g. device timestamp)
+    def update(self, timestamp=None):
+        import time
+        if timestamp is None: timestamp = time.monotonic()
+        # When called first, just store the timestamp and return
+        if self.timePrev is None:
+            self.timePrev = timestamp
+            return
+        self.count += 1
+        tdiff = timestamp - self.timePrev
+        if tdiff >= self.interval:
+            self.fps = self.count / tdiff
+            if self.name:
+                print(f'FPS {self.name}: {self.fps:.2f}')
+            self.count = 0
+            self.timePrev = timestamp
+
+    def get(self):
+        return self.fps
+
 # Connect to device and start pipeline
 with dai.Device(pipeline) as device:
 
@@ -30,8 +60,12 @@ with dai.Device(pipeline) as device:
     # Output queue will be used to get the rgb frames from the output defined above
     qRgb = device.getOutputQueue(name="rgb", maxSize=4, blocking=False)
 
+    fpsHost   = FPS(name='host  ')
+    fpsDevice = FPS(name='device')
     while True:
         inRgb = qRgb.get()  # blocking call, will wait until a new data has arrived
+        fpsHost.update()
+        fpsDevice.update(inRgb.getTimestamp().total_seconds())
 
         # Retrieve 'bgr' (opencv format) frame
         cv2.imshow("rgb", inRgb.getCvFrame())
