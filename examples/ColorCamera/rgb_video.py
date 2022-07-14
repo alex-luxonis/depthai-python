d3db539e8027914d1290c8321c97177cee31978c
alex-luxonis <60824841+alex-luxonis@users.noreply.github.com>
2022-07-14 19:56:14 +0300

    rgb_video.py: options for a different tuning blob, optimized image noise levels
    Example usage with OV9782:
    
    `python3 rgb_video.py -tun ~/Downloads/tuning_color_ov9782_wide_fov.bin -opt`
    
    With the file downloaded from:
    https://artifacts.luxonis.com/artifactory/luxonis-depthai-data-local/misc/tuning_color_ov9782_wide_fov.bin

--- examples/ColorCamera/rgb_video.py
+++ examples/ColorCamera/rgb_video.py
@@ -2,9 +2,21 @@
 
 import cv2
 import depthai as dai
+import argparse
+from pathlib import Path
+
+parser = argparse.ArgumentParser()
+parser.add_argument('-tun', '--cameraTuning', type=Path, 
+                    help="Path to camera tuning blob to use, overriding the built-in tuning")
+parser.add_argument('-opt', '--optimizeNoise', action='store_true', 
+                    help="Optimize camera noise levels")
+args = parser.parse_args()
 
 # Create pipeline
 pipeline = dai.Pipeline()
+if args.cameraTuning:
+    print('Overriding camera tuning with:', args.cameraTuning)
+    pipeline.setCameraTuningBlobPath(args.cameraTuning)
 
 # Define source and output
 camRgb = pipeline.create(dai.node.ColorCamera)
@@ -15,7 +27,16 @@ xoutVideo.setStreamName("video")
 # Properties
 camRgb.setBoardSocket(dai.CameraBoardSocket.RGB)
 camRgb.setResolution(dai.ColorCameraProperties.SensorResolution.THE_1080_P)
-camRgb.setVideoSize(1920, 1080)
+
+if args.optimizeNoise:
+    print('Optimizing camera noise, by tweaking sharpness and denoise controls')
+    camRgb.initialControl.setSharpness(0)     # range: 0..4, default: 1
+    camRgb.initialControl.setLumaDenoise(0)   # range: 0..4, default: 1
+    camRgb.initialControl.setChromaDenoise(4) # range: 0..4, default: 1
+
+# Not needed to set video size, unless cropping.
+# Would crash with sensors like OV9782 that have a lower resolution...
+#camRgb.setVideoSize(1920, 1080)
 
 xoutVideo.input.setBlocking(False)
 xoutVideo.input.setQueueSize(1)
