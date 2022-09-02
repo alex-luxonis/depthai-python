bab75325b5f5dee3617914685bb6d8482b23c242
alex-luxonis <60824841+alex-luxonis@users.noreply.github.com>
2022-09-03 02:54:15 +0300

    mono_preview.py: print jitter and timestamp diff for Left/Right/Vertical

--- examples/MonoCamera/mono_preview.py
+++ examples/MonoCamera/mono_preview.py
@@ -3,6 +3,8 @@
 import cv2
 import depthai as dai
 
+print("Press C to capture a set of frames, Q to quit")
+
 # Create pipeline
 pipeline = dai.Pipeline()
 
@@ -35,19 +37,50 @@ monoVertical.out.link(xoutVertical.input)
 with dai.Device(pipeline) as device:
 
     # Output queues will be used to get the grayscale frames from the outputs defined above
-    qLeft = device.getOutputQueue(name="left", maxSize=4, blocking=False)
-    qRight = device.getOutputQueue(name="right", maxSize=4, blocking=False)
-    qVertical = device.getOutputQueue(name="vertical", maxSize=4, blocking=False)
+    qLeft = device.getOutputQueue(name="left", maxSize=30, blocking=True)
+    qRight = device.getOutputQueue(name="right", maxSize=30, blocking=True)
+    qVertical = device.getOutputQueue(name="vertical", maxSize=30, blocking=True)
 
+    cnt = 0
+    prevL, prevR, prevV = 0, 0, 0
+    tFrame = 1 / monoLeft.getFps()  # 0.033s, for 30fps
+    print("Diff: L-prevL-FT R-prevR-FT V-prevV-FT L-R L-V   (FT=frametime)")
     while True:
         # Instead of get (blocking), we use tryGet (non-blocking) which will return the available data or None otherwise
-        inLeft = qLeft.get()
-        inRight = qRight.get()
-        inVertical = qVertical.get()
+        inL = qLeft.get()
+        inR = qRight.get()
+        inV = qVertical.get()
+        tstampL = inL.getTimestamp().total_seconds()
+        tstampR = inR.getTimestamp().total_seconds()
+        tstampV = inV.getTimestamp().total_seconds()
+        if 0:
+            print(f'>>> L:{inL.getSequenceNum()} t:{tstampL}')
+            print(f'    R:{inR.getSequenceNum()} t:{tstampR}')
+            print(f'    V:{inV.getSequenceNum()} t:{tstampV}')
+        jitterL = (tstampL - prevL - tFrame) * 1000
+        jitterR = (tstampR - prevR - tFrame) * 1000
+        jitterV = (tstampV - prevV - tFrame) * 1000
+        diffLR = (tstampL - tstampR) * 1000
+        diffLV = (tstampL - tstampV) * 1000
+        print(f'{jitterL:7.3f} {jitterR:7.3f} {jitterV:7.3f} {diffLR:6.3f} {diffLV:6.3f}')
+        prevL = tstampL
+        prevR = tstampR
+        prevV = tstampV
 
-        cv2.imshow("left", inLeft.getCvFrame())
-        cv2.imshow("right", inRight.getCvFrame())
-        cv2.imshow("vertical", inVertical.getCvFrame())
+        frameL = inL.getCvFrame()
+        frameR = inR.getCvFrame()
+        frameV = inV.getCvFrame()
 
-        if cv2.waitKey(1) == ord('q'):
+        cv2.imshow("left", frameL)
+        cv2.imshow("right", frameR)
+        cv2.imshow("vertical", frameV)
+
+        key = cv2.waitKey(1)
+        if key == ord('q'):
             break
+        elif 0 or key == ord('c'):
+            print("Saving, iter", cnt)
+            cv2.imwrite(str(cnt)+"_left_"    +str(tstampL)+".png", frameL)
+            cv2.imwrite(str(cnt)+"_right_"   +str(tstampR)+".png", frameR)
+            cv2.imwrite(str(cnt)+"_vertical_"+str(tstampV)+".png", frameV)
+            cnt += 1
