d2e8a75294ca39c1cf2a3a77c81ed710064705f4
alex-luxonis <60824841+alex-luxonis@users.noreply.github.com>
2022-11-12 00:39:50 +0200

    rgb_preview.py: inverted (negative) image, in Script node, slow...

--- examples/ColorCamera/rgb_preview.py
+++ examples/ColorCamera/rgb_preview.py
@@ -17,8 +17,27 @@ camRgb.setPreviewSize(300, 300)
 camRgb.setInterleaved(False)
 camRgb.setColorOrder(dai.ColorCameraProperties.ColorOrder.RGB)
 
+script = pipeline.create(dai.node.Script)
+script.setScript("""
+    while True:
+        f = node.io['in'].get()
+
+        w, h, d = f.getWidth(), f.getHeight(), f.getData()
+        node.warn(f'{w}x{h}, size {len(d)}')
+
+        if 0: # invert the color of first pixel
+            for i in range(3): # 3 planes
+                d[w*h*i] = 255 - d[w*h*i]
+        else: # invert full image in-place
+            for i in range(len(d)):
+                d[i] = 255 - d[i]
+
+        node.io['out'].send(f)
+""")
+
 # Linking
-camRgb.preview.link(xoutRgb.input)
+camRgb.preview.link(script.inputs['in'])
+script.outputs['out'].link(xoutRgb.input)
 
 # Connect to device and start pipeline
 with dai.Device(pipeline) as device:
